b11a48758317859859065b2cdc3f50bfd2c3fc5c
Kha Doan <117446709+KhaDoan-STU@users.noreply.github.com>
2022-12-12 15:33:28 +0700

    hot-fix back to top (#22)

--- src/Components/BackToTop/ScrollButton.jsx
+++ src/Components/BackToTop/ScrollButton.jsx
@@ -1,10 +1,34 @@
 import React, { useState } from 'react';
 import BackToTop from 'react-back-to-top-button';
-import './ScrollButton.scss';
 const ScrollButton = () => {
+  const [background, setBg] = useState('#00aeef');
+
+  const btnStyle = {
+    margin: '0',
+    right: '30px',
+    bottom: '30px',
+    fontSize: '18px',
+    background: background,
+    border: 'none',
+    width: '40px',
+    height: '40px',
+    lineHeight: '40px',
+    borderRadius: '4px',
+    zIndex: '999',
+    transition: '0.2s',
+    color: 'white',
+  };
   return (
-    <BackToTop showAt={100} speed={500} easing='easeInOutSine'>
-      <i class='fa-solid fa-angle-up'></i>
+    <BackToTop style={btnStyle} showAt={100} speed={500} easing='easeInOutSine'>
+      <i
+        onMouseOver={() => {
+          setBg('#23baf0');
+        }}
+        onMouseOut={() => {
+          setBg('#00aeef');
+        }}
+        class='fa-solid fa-angle-up'
+      ></i>
     </BackToTop>
   );
 };
