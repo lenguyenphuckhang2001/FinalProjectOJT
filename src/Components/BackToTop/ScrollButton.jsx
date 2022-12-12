7c55c1877a62e00f51ecaeb8a61293667b9535e2
Kha Doan <117446709+KhaDoan-STU@users.noreply.github.com>
2022-12-12 16:18:45 +0700

    kha hot-fix scroll button update (#24)

--- src/Components/BackToTop/ScrollButton.jsx
+++ src/Components/BackToTop/ScrollButton.jsx
@@ -1,36 +1,37 @@
-import React, { useState } from 'react';
-import BackToTop from 'react-back-to-top-button';
+import React, { useEffect, useState } from 'react';
+import './ScrollButton.scss';
 const ScrollButton = () => {
-  const [background, setBg] = useState('#00aeef');
+  const [showBtn, setTopbtn] = useState(false);
 
-  const btnStyle = {
-    margin: '0',
-    right: '30px',
-    bottom: '30px',
-    fontSize: '18px',
-    background: background,
-    border: 'none',
-    width: '40px',
-    height: '40px',
-    lineHeight: '40px',
-    borderRadius: '4px',
-    zIndex: '999',
-    transition: '0.2s',
-    color: 'white',
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
   };
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        setTopbtn(true);
+      } else {
+        setTopbtn(false);
+      }
+    });
+  }, []);
+
   return (
-    <BackToTop style={btnStyle} showAt={100} speed={500} easing='easeInOutSine'>
-      <i
-        onMouseOver={() => {
-          setBg('#23baf0');
-        }}
-        onMouseOut={() => {
-          setBg('#00aeef');
-        }}
-        class='fa-solid fa-angle-up'
-      ></i>
-    </BackToTop>
+    <div>
+      {showBtn ? (
+        <div className='scrollToTopOn' onClick={scrollToTop}>
+          <i class='fa-sharp fa-solid fa-angle-up'></i>
+        </div>
+      ) : (
+        <div className='scrollToTopOff' onClick={scrollToTop}>
+          <i class='fa-sharp fa-solid fa-angle-up'></i>
+        </div>
+      )}
+    </div>
   );
 };
-
 export default ScrollButton;
